b74c957310aa3f8aeedc8d619e4e6dfdd8eecb6b
Семен Шевчик <semyonshevchik@MacBook-Air-Semen.local>
2023-09-20 14:30:52 +0300

    Initial commit

---
+++ HomeWorkSchevchik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkSchevchik
+//
+//  Created by Семен Шевчик on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
